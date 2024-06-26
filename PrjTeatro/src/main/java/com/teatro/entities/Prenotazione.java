afdf2198dd9201246b5dadf6cfe39c8da6b51833
Stefania Neri <stefania.neri.92@gmail.com>
2024-06-26 20:54:31 +0200

    works

--- PrjTeatro/src/main/java/com/teatro/entities/Prenotazione.java
+++ PrjTeatro/src/main/java/com/teatro/entities/Prenotazione.java
@@ -1,5 +1,23 @@
 package com.teatro.entities;
 
+import java.sql.Date;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Prenotazione {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+    private String nomeCliente;
+    private Date dataPrenotazione;
+    private Replica replica;
+    private List<Biglietto> biglietti;
 
 }
