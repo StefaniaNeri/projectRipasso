2da52c9b687530388da2d41120187a743957e4db
Stefania Neri <stefania.neri.92@gmail.com>
2024-07-21 13:37:55 +0200

    banking

---
+++ PrjBanking/src/main/java/com/banking/PrjBankingApplication.java
@@ -0,0 +1,13 @@
+package com.banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjBankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrjBankingApplication.class, args);
+	}
+
+}
---
+++ PrjBanking/src/main/java/com/banking/entities/Utente.java
@@ -0,0 +1,5 @@
+package com.banking.entities;
+
+public class Utente {
+
+}
