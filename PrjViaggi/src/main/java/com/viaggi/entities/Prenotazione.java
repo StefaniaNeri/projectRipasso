f8618df471d34e6b04fa29a40281c73bac97fe28
Stefania Neri <stefania.neri.92@gmail.com>
2024-07-22 21:37:49 +0200

    .

--- PrjViaggi/src/main/java/com/viaggi/entities/Prenotazione.java
+++ PrjViaggi/src/main/java/com/viaggi/entities/Prenotazione.java
@@ -1,8 +1,30 @@
 package com.viaggi.entities;
 
+import java.sql.Date;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Prenotazione {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private Date dataPrenotazione;
+	
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name= "id_viaggio")
+	private Viaggio viaggio;
+	
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name= "id_utente")
+	private Utente utente;
 
 }
--- PrjViaggi/src/main/java/com/viaggi/entities/Utente.java
+++ PrjViaggi/src/main/java/com/viaggi/entities/Utente.java
@@ -1,8 +1,25 @@
 package com.viaggi.entities;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 
 @Entity
 public class Utente {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id_utente;
+	
+	private String nome, cognome, username, password;
+	
+	@OneToMany(mappedBy = "utente", cascade = CascadeType.ALL)
+	private Set<Prenotazione> prenotazioni = new HashSet<Prenotazione>();
 
 }
