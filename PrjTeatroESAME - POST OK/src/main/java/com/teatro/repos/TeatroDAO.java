c5e8c77c7dfc6fb38c1ce70ea42a926ad706a5e9
Stefania Neri <117984737+StefaniaNeri@users.noreply.github.com>
2024-07-31 22:54:27 +0200

    Delete PrjTeatroESAME - POST OK directory

--- PrjTeatroESAME - POST OK/src/main/java/com/teatro/repos/TeatroDAO.java
+++
@@ -1,9 +0,0 @@
-package com.teatro.repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.teatro.entities.Teatro;
-
-public interface TeatroDAO extends JpaRepository<Teatro, String> {
-
-}
